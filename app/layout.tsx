fb4f1d03c8844aeb0d50eb6d161bd5c6b9593869
beanduong <pagrafy@gmail.com>
2024-06-03 20:36:24 +0200

    refactor: Update image handling and add blur placeholder in page.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,10 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "FAMFAM Demo",
+  title: "FAMFAM",
+  description:
+    "FAMFAM's mission is to create a harmonious community where electronic music lovers can thrive together, embodying the essence of a family within a family. Join us to foster a full-circle ecosystem of giving and receiving, underpinned by trust, karma, and the genuine desire to do good.",
+  authors: [{ name: "FAMFAM" }],
 };
 
 export default function RootLayout({
